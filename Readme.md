b76ed037eb4b20c16b76eca118954c05d4cf3e35
Evan Cordell <evan.cordell@localmed.com>
2015-05-24 17:53:04 -0500

    Add deserialization

--- Readme.md
+++ Readme.md
@@ -35,5 +35,16 @@ signature 007692a3aa3e4bf1967a2650d19885970a3d5137abb043ca628ea6e0b47d8ce4
 > let mf = addFirstPartyCaveat m "predicate"
 > serialize mf
 
-"MDAwZGlkZW50aWZpZXIgaWQKMDAwY2xvY2F0aW9uIGxvYwowMDA0Y2lkIApwcmVkaWNhdGUwMDRhc2lnbmF0dXJlIDAwNzY5MmEzYWEzZTRiZjE5NjdhMjY1MGQxOTg4NTk3MGEzZDUxMzdhYmIwNDNjYTYyOGVhNmUwYjQ3ZDhjZTQK"
+"MDAwZGlkZW50aWZpZXIgaWQKMDAwY2xvY2F0aW9uIGxvYwowMDBkY2lkIHByZWRpY2F0ZQowMDJhc2lnbmF0dXJlIAB2kqOqPkvxlnomUNGYhZcKPVE3q7BDymKOpuC0fYzkCg"
+```
+
+# Deserializing
+
+```haskell
+> let m = deserialize "MDAwZGlkZW50aWZpZXIgaWQKMDAwY2xvY2F0aW9uIGxvYwowMDBkY2lkIHByZWRpY2F0ZQowMDJhc2lnbmF0dXJlIAB2kqOqPkvxlnomUNGYhZcKPVE3q7BDymKOpuC0fYzkCg"
+> putStrLn $ inspect m
+
+identifier id
+location loc
+signature be9ae46bcfba7353d9dd29afd9686c11b8539a71cffa25b2a1af5a1e9180171b
 ```
--- src/Macaroon/Macaroon.hs
+++ src/Macaroon/Macaroon.hs
@@ -5,10 +5,15 @@ import Data.Digest.Pure.SHA
 import qualified Data.ByteString.Lazy.Char8 as C
 import Text.Printf
 import qualified Data.ByteString.Base64.URL.Lazy as Base64
+import qualified Data.ByteString.Base16.Lazy as Hex
+import Data.Binary.Get
+import Data.Word
+import Data.Char
+import Data.Bits
 
 type Identifier = String
 type Location = String
-type Signature = Digest SHA256State
+type Signature = C.ByteString
 type Key = String
 
 data Macaroon = Macaroon { identifier :: Identifier
@@ -17,23 +22,22 @@ data Macaroon = Macaroon { identifier :: Identifier
                          , signature :: Signature
                          } deriving (Show)
 
-
 baseMacaroon :: Key -> Identifier -> Location -> Macaroon
 baseMacaroon key id loc = Macaroon id loc [] $ initialSignature key id
 
 bytestringSignature :: Macaroon -> C.ByteString
-bytestringSignature = bytestringDigest . signature
+bytestringSignature = signature
 
 showSignature :: Macaroon -> String
-showSignature = showDigest . signature
+showSignature = C.unpack . Hex.encode . signature
 
 initialSignature :: Key -> Identifier -> Signature
-initialSignature key id = hmacSha256 derivedKey packedId
-  where derivedKey = bytestringDigest $ generateDerivedKey key
+initialSignature key id = bytestringDigest $ hmacSha256 derivedKey packedId
+  where derivedKey = generateDerivedKey key
         packedId = C.pack id
 
 generateDerivedKey :: Key -> Signature
-generateDerivedKey key = hmacSha256 seed packedKey
+generateDerivedKey key = bytestringDigest $ hmacSha256 seed packedKey
   where seed = C.pack "macaroons-key-generator"
         packedKey = C.pack key
 
@@ -51,31 +55,79 @@ addFirstPartyCaveat macaroon predicate =
   where caveat = FirstPartyCaveat predicate
 
 signFirstPartyCaveat :: Macaroon -> Caveat -> Signature
-signFirstPartyCaveat currentSig caveat = hmacSha256 packedCurrentSig packedCaveat
-  where packedCurrentSig = bytestringSignature currentSig
+signFirstPartyCaveat m caveat = bytestringDigest $ hmacSha256 currentSig packedCaveat
+  where currentSig = signature m
         packedCaveat = C.pack $ caveatId caveat
 
 serialize :: Macaroon -> String
-serialize = trimBase64. C.unpack . Base64.encode . C.pack . formatBinary
+serialize = trimBase64 . C.unpack . Base64.encode . C.pack . formatBinary
+
+deserialize :: String -> Macaroon
+deserialize = parseBinary . C.unpack . Base64.decodeLenient . C.pack . padBase64
 
 formatBinary :: Macaroon -> String
 formatBinary m = packetize ("identifier " ++ identifier m) ++
               packetize ("location " ++ location m) ++
               caveatPackets ++
-              packetize ("signature " ++ showDigest (signature m))
+              packetize ("signature " ++ C.unpack (signature m))
   where caveatPackets = foldl (\acc c -> acc ++ packetizeCaveat c) "" $ caveats m
+
+parseBinary :: String -> Macaroon
+parseBinary s = parseBinaryHelper blankMacaroon (runGet listOfPackets (C.pack s))
+  where blankMacaroon = Macaroon "" "" [] $ initialSignature "" ""
+
+parseBinaryHelper :: Macaroon -> [C.ByteString] -> Macaroon
+parseBinaryHelper m (current:rest)
+    | label == C.pack "identifier" = parseBinaryHelper (m {identifier = C.unpack value}) rest
+    | label == C.pack "location" = parseBinaryHelper (m {location = C.unpack value}) rest
+    | label == C.pack "cid" = parseBinaryHelper (m {caveats = (caveats m) ++ [caveat]}) rest
+    | label == C.pack "signature" = parseBinaryHelper (m {signature = value}) rest
+    | otherwise = parseBinaryHelper m rest
+    where [label, value] = C.split ' ' current
+          caveat = parseCaveat value rest
+
+parseBinaryHelper m [] = m
+
+parseCaveat :: C.ByteString -> [C.ByteString] -> Caveat
+parseCaveat cid rest
+  | vidLabel == C.pack "vid" = ThirdPartyCaveat (C.unpack cid) (C.unpack vidValue) (C.unpack clValue)
+  | otherwise = FirstPartyCaveat (C.unpack cid)
+  where [vidLabel, vidValue] = C.split ' ' (head rest)
+        [clLabel, clValue] = C.split ' ' (head (tail rest))
+
+listOfPackets :: Get [C.ByteString]
+listOfPackets = do
+  empty <- isEmpty
+  if empty
+     then return []
+     else do len <- getLazyByteString 4
+             packet <- getLazyByteString (fromIntegral (hexToInt len))
+             skip 1 -- newline
+             rest <- listOfPackets
+             return (packet : rest)
+
+hexToInt :: C.ByteString -> Int
+hexToInt hex = C.foldl' f 0 hex
+  where f n c = 16*n + digitToInt c
 
 packetize :: String  -> String
 packetize s = (printf "%04x" (length s)) ++ s ++ "\n"
 
 packetizeCaveat :: Caveat  -> String
-packetizeCaveat (FirstPartyCaveat p) = packetize "cid " ++ p
-packetizeCaveat (ThirdPartyCaveat p loc id) = (packetize "cid " ++ p) ++
-                                              (packetize "vid " ++ id) ++
-                                              (packetize "cl " ++ loc)
+packetizeCaveat (FirstPartyCaveat p) = packetize $ "cid " ++ p
+packetizeCaveat (ThirdPartyCaveat p loc id) = (packetize $ "cid " ++ p) ++
+                                              (packetize $ "vid " ++ id) ++
+                                              (packetize $ "cl " ++ loc)
 
 trimBase64 :: String -> String
 trimBase64 = reverse . dropWhile isEqualSign . reverse
+
+padBase64 :: String -> String
+padBase64 s
+  | len `mod` 4 == 0 = s
+  | otherwise = s ++ padding
+  where len = length s
+        padding = replicate ((-len) `mod` 4) '='
 
 isEqualSign :: Char -> Bool
 isEqualSign c
